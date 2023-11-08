620ccb04fa9424662091b1a850ee2312a0caece5
Robbie Buxton <robbiesbuxton@gmail.com>
2023-11-08 00:02:40 +0000

    added basic azure kinect sensor dep
    Signed-off-by: Robbie Buxton <robbiesbuxton@gmail.com>

--- flake.nix
+++ flake.nix
@@ -3,12 +3,14 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/23.05";
+    k4a.url = "github:RobbieBuxton/Azure-Kinect-Sensor-SDK/git-submodule-fix";
   };
 
-  outputs = { self, nixpkgs }: 
+  outputs = { self, nixpkgs, k4a}: 
   let 
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
+    
   in
   {
   # Development shell used by running "nix develop".
@@ -16,21 +18,23 @@
   devShells.${system}.default = pkgs.mkShell rec {
     name = "volumetricSim";
 
-    packages = with pkgs; [ 
+    packages = [ 
+      k4a.packages.${system}.default
+
       #Setup and windown
-      gcc
-      python310Packages.glad2
-      glxinfo
-      killall
-      jq
+      pkgs.gcc
+      pkgs.python310Packages.glad2
+      pkgs.glxinfo
+      pkgs.killall
+      pkgs.jq
       
       # Libs
-      glfw
-      glm
-      stb
-      xorg.libX11
-      xorg.libXrandr
-      xorg.libXi
+      pkgs.glfw
+      pkgs.glm
+      pkgs.stb
+      pkgs.xorg.libX11
+      pkgs.xorg.libXrandr
+      pkgs.xorg.libXi
     ];
 
     shellHook = let
@@ -65,45 +69,132 @@
     '';
   };
 
-    packages.${system}.default = pkgs.stdenv.mkDerivation {
-      pname = "volumetricSim";
-      version = "0.0.1";
+    packages.${system} = {
+      default = pkgs.stdenv.mkDerivation {
+        pname = "volumetricSim";
+        version = "0.0.1";
 
-      src = ./.; 
+        src = ./.; 
 
-      buildInputs = with pkgs; [
-        python310Packages.glad2
-        glxinfo
+        buildInputs = with pkgs; [
+          python310Packages.glad2
+          glxinfo
 
-        # Libs
-        glfw
-        glm
-        stb
-        xorg.libX11
-        xorg.libXrandr
-        xorg.libXi
-      ];
+          # Libs
+          glfw
+          glm
+          stb
+          xorg.libX11
+          xorg.libXrandr
+          xorg.libXi
+        ];
 
-     currentPath = toString ./.;
+      currentPath = toString ./.;
 
-      buildPhase = let
-        gccBuildLibs = "-lglfw -lGL -lX11 -lpthread -lXrandr -lXi -ldl -lm";
-        openGLVersion = "glxinfo | grep -oP '(?<=OpenGL version string: )[0-9]+.?[0-9]'";
-        gladBuildDir = "build/glad";
-        sourceFiles = "src/main.cpp ${gladBuildDir}/src/gl.c";
-        includePaths = "-I ${gladBuildDir}/include -I include";
-      in ''
-        glad --api gl:core=`${openGLVersion}` --out-path ${gladBuildDir} --reproducible 
-        echo ${includePaths}
-        g++ ${gccBuildLibs} ${sourceFiles} ${includePaths} -o volumetricSim 
-      '';
+        buildPhase = let
+          gccBuildLibs = "-lglfw -lGL -lX11 -lpthread -lXrandr -lXi -ldl -lm";
+          openGLVersion = "glxinfo | grep -oP '(?<=OpenGL version string: )[0-9]+.?[0-9]'";
+          gladBuildDir = "build/glad";
+          sourceFiles = "src/main.cpp ${gladBuildDir}/src/gl.c";
+          includePaths = "-I ${gladBuildDir}/include -I include";
+        in ''
+          glad --api gl:core=`${openGLVersion}` --out-path ${gladBuildDir} --reproducible 
+          echo ${includePaths}
+          g++ ${gccBuildLibs} ${sourceFiles} ${includePaths} -o volumetricSim 
+        '';
 
-      installPhase = ''
-        mkdir -p $out/bin
-        cp volumetricSim $out/bin
-      '';
-    };
+        installPhase = ''
+          mkdir -p $out/bin
+          cp volumetricSim $out/bin
+        '';
+      };
+
+      # k4a =
+      # let 
+      #   azure-kinect-sensor-sdk = builtins.fetchGit {
+      #     url = "https://github.com/RobbieBuxton/Azure-Kinect-Sensor-SDK/";
+      #     rev = "eb1e6ddca3952774c07a2a5bfcb55e1241968bda";
+      #     submodules = true;
+      #   };  
+      #   depthengine = builtins.fetchurl {
+      #     url = https://packages.microsoft.com/ubuntu/18.04/prod/pool/main/libk/libk4a1.4/libk4a1.4_1.4.1_amd64.deb;
+      #     sha256 = "sha256:0ackdiakllmmvlnhpcmj2miix7i2znjyai3a2ck17v8ycj0kzin1";
+      #   };   
+      # in 
+      # pkgs.stdenv.mkDerivation {
+      #   pname = "k4aviewer";
+      #   version = "1.4.1";
+
+      #   src = azure-kinect-sensor-sdk; 
+
+      #   #Stops cmake killing itself
+      #   dontUseCmakeConfigure = true;
+      #   dontUseCmakeBuildDir = true; 
+      #   nativeBuildInputs = with pkgs; [
 
     
+      #   ];
+
+      #   buildInputs = with pkgs; [
+      #     #Try and fix build libs
+      #     git
+      #     patchelf
+      #     gnused
+
+      #     #needed bibs
+      #     cmake
+      #     pkg-config
+      #     ninja
+      #     doxygen
+      #     python312
+      #     nasm
+      #     dpkg
+
+      #     #Maybe need libs
+      #     glfw
+      #     xorg.libX11
+      #     xorg.libXrandr
+      #     xorg.libXinerama
+      #     xorg.libXcursor
+      #     openssl_legacy
+      #     libsoundio
+      #     libusb1
+      #     libjpeg
+      #     opencv
+      #     libuuid
+      #   ];
+
+      #   configurePhase = ''
+      #     mkdir -p build/bin 
+      #     dpkg -x ${depthengine} build/libdepthengine
+      #     cp build/libdepthengine/usr/lib/x86_64-linux-gnu/libk4a1.4/libdepthengine.so.2.0 build/bin/
+      #     rm -r build/libdepthengine
+      #     cd build
+      #     cmake .. -GNinja
+      #   '';
+
+      #   buildPhase = ''
+      #     ninja
+      #     export BUILD=`pwd`
+      #   '';
+
+      #   installPhase = ''
+      #     mkdir -p $out/bin
+      #     cp -r bin $out
+      #     mkdir -p $out/include
+      #     cp -r ../include/k4a $out/include/
+      #   '';
+      
+      #   #Removes any RPATH refrences to the temp build folder used during the configure and install phase
+      #   fixupPhase = 
+      #   let
+      #     removeRPATH = file: path: "patchelf --set-rpath `patchelf --print-rpath ${file} | sed 's@'${path}'@@'` ${file}";
+      #   in ''
+      #     cd $out/bin
+      #     for f in *; do if [[ "$f" =~ .*\..*  ]]; then : ignore;else ${removeRPATH "$f" "$BUILD/bin:"};fi; done
+      #     ${removeRPATH "libk4arecord.so.1.4.0" "$BUILD/bin:"}
+      #   '';
+      # };
+    };
   };
 }
